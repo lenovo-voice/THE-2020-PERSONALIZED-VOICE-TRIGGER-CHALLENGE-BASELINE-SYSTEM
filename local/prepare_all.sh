110d6039b32d5680c3bcae36c48ce7a2a5cfac05
jiay7 <yanjia150124@outlook.com>
2020-12-19 17:00:32 +0800

    init

--- local/prepare_all.sh
+++ local/prepare_all.sh
@@ -11,11 +11,11 @@ testset_task2=$5
 if [ $stage -le 1 ];then
 	echo "local/prepare_all.sh"
 	cd ./src
-	#echo $PWD
-	#python prepare_data.py --dataset_path $train_set --dest_path data/all_train || exit 1
-	#python cut_keyword_wav.py --split_file $split_file --wav_file data/all_train/wav.scp --save_dir data/split_wav/ || exit 1
-	#python combine_data.py --split_wav_dir data/split_wav/ --data_dir data/all_train --combine_data_dir data/PVTC	|| exit 1
-	#python prepare_task_data.py --dev_dataset $dev_set --dest_dir ../task/ || exit 1
+	echo $PWD
+	python prepare_data.py --dataset_path $train_set --dest_path data/all_train || exit 1
+	python cut_keyword_wav.py --split_file $split_file --wav_file data/all_train/wav.scp --save_dir data/split_wav/ || exit 1
+	python combine_data.py --split_wav_dir data/split_wav/ --data_dir data/all_train --combine_data_dir data/PVTC	|| exit 1
+	python prepare_task_data.py --dev_dataset $dev_set --dest_dir ../task/ || exit 1
 	./prepare_testset_data.sh $testset_task1 ../testset_task1
 	./prepare_testset_data.sh $testset_task2 ../testset_task2	
 	cd ../
